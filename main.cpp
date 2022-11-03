ece1e23fdd56b7cc61ed11f0dd12717ad7c94415
Konrad Szczurek <student@it11-17.it.local>
2022-11-03 12:22:25 +0100

    Wersja 1.0

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout << "Obliczanie miejsc zerowych" << endl;
+	float a=2;
+	float b=3;
+	float x=-b/a;
+	cout << x;
+	return 0;
+}
