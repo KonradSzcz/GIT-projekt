08391ddb03adbbaca7dc6f9625fb2fd69218064e
Konrad Szczurek <student@it11-17.it.local>
2022-11-03 13:18:46 +0100

    wersja 2.2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	float a,b,c,d,e,f,delta,x1,x2;
+	float a,b,c,delta,x1,x2;
+	
+	
+	int funkcja;
+	cout << "Wybierz funkcje"<<endl;
+	cout << "1 liniowa"<<endl<<"2 kwadratowa"<<endl;
+	cin >> funkcja;
+	if (funkcja == 1){
+		
 	cout << "Wybierz postac funkcji"<<endl;
 	cout << "1 zwyk³a"<<endl<<"2 ogolna"<<endl;
 	int postac;
@@ -44,19 +52,43 @@ int main(int argc, char** argv) {
 			cout << "Niesokonczenie wiele miejsc";
 		}
 	}
-	cout << "Funkcja kwadratowa";
+}
+else if (funkcja == 2) {
+	
+
+	cout << "Funkcja kwadratowa"<<endl;		
 		cout << "Podaj a"<<endl;
-		cin >> d;
+		cin >> a;
 		cout << "Podaj b"<<endl;
-		cin >> e;
+		cin >> b;
 		cout << "Podaj c"<<endl;
-		cin >> f;
-		delta = e*e-(4*d*f);
+		cin >> c;
+	if(a==0){
+		cout << "Wynik"<<endl;
+		float x1=-b/a;
+		cout << x1;
+	}
+	else if (a!=0) {
+
+		delta = b*b-(4*a*c);
+		if (delta > 0 ){
+	
 		float delta1 = sqrt(delta);
 		cout << "Miejsca zerowe"<<endl;
 		x1=(-b-delta1)/(2*a);
 		x2=(-b+delta1)/(2*a);
 		cout << "x1:"<<x1<<" , "<<"x2:"<<x2;
+			}
+		else if (delta ==0){
+			x1=-b/(2*a);
+			cout << "Miejsce zerowe"<<endl;
+			cout << x1;
+		}
 		
+		else if (delta < 0) {
+			cout << "Brak";
+		}			
+}	
+}
 	return 0;
 }
